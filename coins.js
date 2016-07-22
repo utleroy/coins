4e7a936f80bacb66121eb052be9474b7edf63e05
Bryan Hankins <bryanhankins@me.com>
2016-07-21 21:35:47 -0500

    fixed pennies output

--- coins.js
+++ coins.js
@@ -1,21 +1,23 @@
   function coinCounter (num) {
   var coinPurse = {};
-    if (num >= 25) {
-      coinPurse.quarters = Math.floor(num/25);
-      num = (num - (coinPurse.quarters*25));
+    if (num >= 0.25) {
+      coinPurse.quarters = Math.floor(num/0.25);
+      num = (num - (coinPurse.quarters*0.25));
     }
-    if (num >= 10 || num <= 24) {
-      coinPurse.dimes = Math.floor(num/10);
-      num = (num - (coinPurse.dimes*10));
+    if (num >= 0.10 || num <= 0.24) {
+      coinPurse.dimes = Math.floor(num/0.10);
+      num = (num - (coinPurse.dimes*0.10));
     }
-    if (num >= 5 || num <=9) {
-      coinPurse.nickels = Math.floor(num/5);
-      num = (num - (coinPurse.nickels*5));
+    if (num >= 0.05 || num <=0.09) {
+      coinPurse.nickels = Math.floor(num/0.05);
+      num = (num - (coinPurse.nickels*0.05));
     }
-    coinPurse.pennies = num;
-
-  return coinPurse;
+    if (num >= 0.01 || num <= 0.04) {
+      coinPurse.pennies = Math.floor(num/0.01);
+      num = (num - (coinPurse.pennies*0.01));
+    }
+  return coinPurse || (num);
 }
 
-var coins = coinCounter(74);
+var coins = coinCounter(0.67);
 console.log(coins);
